f970fef2f75683df9ad657bfbc9402d2e649dd4a
ardakoc <ardaskoc@gmail.com>
2022-05-30 17:38:25 +0300

    listing of tasks and task operations have been customized for the user

--- base/views.py
+++ base/views.py
@@ -22,6 +22,13 @@ class TaskList(LoginRequiredMixin, ListView):
   model = Task
   context_object_name = 'tasks'
 
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['tasks'] = context['tasks'].filter(user=self.request.user)
+    context['count'] = context['tasks'].filter(complete=False).count()
+
+    return context
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
   model = Task
@@ -31,14 +38,22 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class CreateTask(LoginRequiredMixin, CreateView):
   model = Task
-  fields = '__all__'
+  fields = ['title', 'description', 'complete']
   success_url = reverse_lazy('tasks')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(CreateTask, self).form_valid(form)
 
 
 class UpdateTask(LoginRequiredMixin, UpdateView):
   model = Task
-  fields = '__all__'
+  fields = ['title', 'description', 'complete']
   success_url = reverse_lazy('tasks')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(UpdateTask, self).form_valid(form)
 
 
 class DeleteTask(LoginRequiredMixin, DeleteView):
