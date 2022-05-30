835078e2c29724f9193e77ba8244fb4fec61433b
ardakoc <ardaskoc@gmail.com>
2022-05-30 16:03:18 +0300

    creating the basic task detail page

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
+from django import views
 from django.urls import path
-from .views import TaskList
+from .views import TaskDetail, TaskList
 
 urlpatterns = [
-  path('', TaskList.as_view(), name='tasks')
+  path('', TaskList.as_view(), name='tasks'),
+  path('task/<int:pk>/', TaskDetail.as_view(), name='task')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from base.models import Task
 
@@ -8,3 +8,9 @@ from base.models import Task
 class TaskList(ListView):
   model = Task
   context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+  model = Task
+  context_object_name = 'task'
+  template_name = 'base/task.html'
