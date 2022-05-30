8e8d9d39aa8b15750889e102827e3b17aa505e55
ardakoc <ardaskoc@gmail.com>
2022-05-30 16:17:22 +0300

    creating the basic task creation page

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django import views
 from django.urls import path
-from .views import TaskDetail, TaskList
+from .views import CreateTask, TaskDetail, TaskList
 
 urlpatterns = [
   path('', TaskList.as_view(), name='tasks'),
-  path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+  path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+  path('create-task/', CreateTask.as_view(), name='create-task')
 ]
