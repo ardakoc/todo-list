0a780187497156f9ae267ff65363251c96afb0b9
ardakoc <ardaskoc@gmail.com>
2022-05-30 16:54:05 +0300

    creating the basic task delete page

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django import views
 from django.urls import path
-from .views import CreateTask, TaskDetail, TaskList, UpdateTask
+from .views import CreateTask, DeleteTask, TaskDetail, TaskList, UpdateTask
 
 urlpatterns = [
   path('', TaskList.as_view(), name='tasks'),
   path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
   path('create-task/', CreateTask.as_view(), name='create-task'),
   path('update-task/<int:pk>/', UpdateTask.as_view(), name='update-task'),
+  path('delete-task/<int:pk>/', DeleteTask.as_view(), name='delete-task')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from base.models import Task
@@ -26,4 +26,10 @@ class CreateTask(CreateView):
 class UpdateTask(UpdateView):
   model = Task
   fields = '__all__'
+  success_url = reverse_lazy('tasks')
+
+
+class DeleteTask(DeleteView):
+  model = Task
+  context_object_name = 'task'
   success_url = reverse_lazy('tasks')
