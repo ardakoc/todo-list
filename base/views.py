361c5a27e6660c459f574a1fc328befedcb22522
ardakoc <ardaskoc@gmail.com>
2022-05-31 16:30:59 +0300

    creating the basic registration page and adding registration functionality

--- base/views.py
+++ base/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from base.models import Task
 
@@ -16,6 +19,24 @@ class Login(LoginView):
 
   def get_success_url(self) -> str:
     return reverse_lazy('tasks')
+
+
+class Register(FormView):
+  template_name = 'base/register.html'
+  form_class = UserCreationForm
+  redirect_authenticated_user = True
+  success_url = reverse_lazy('tasks')
+
+  def form_valid(self, form):
+    user = form.save()
+    if user is not None:
+      login(self.request, user)
+    return super(Register, self).form_valid(form)
+
+  def get(self, *args, **kwargs):
+    if self.request.user.is_authenticated:
+      return redirect('tasks')
+    return super(Register, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
